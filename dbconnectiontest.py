e2eedb679423f90ea6a4362d3c6bfc349aebbb06
gokkuu100 <wkurts247@gmail.com>
2024-06-30 19:35:50 +0300

    added to ignore

--- dbconnectiontest.py
+++ dbconnectiontest.py
@@ -3,9 +3,9 @@ import pymysql
 try:
     connection = pymysql.connect(
         host='vitapharmdb-1.cj6ico0sizn7.eu-north-1.rds.amazonaws.com',
-        user='admin',
-        password='vitapharm100',
-        database='vitapharm'
+        user='',
+        password='',
+        database=''
     )
     print("Connection successful")
 except Exception as e:
